b3eb00d38715200b9ffc041729ccaf35ac14271f
Nicoffb <nicoffb@hotmail.com>
2023-02-10 10:07:30 +0100

    primer commit

---
+++ src/main/java/com/salesianostriana/gamesforall/GamesforallApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.gamesforall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesforallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesforallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/gamesforall/GamesforallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.gamesforall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesforallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
